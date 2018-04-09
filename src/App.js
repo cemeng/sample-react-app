0792d175bb809db90c5df60c2bf42c1cd8ace1e2
Felix Tjandrawibawa <cemeng@gmail.com>
2018-04-09 22:00:08 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// stateless function
+// const App = () => <h1>Hello Stateless</h1>
+
+// class - can maintain state
+class App extends React.Component {
+  render(){
+    return <h1>Hello World</h1>
+  }
+}
+
+export default App;
